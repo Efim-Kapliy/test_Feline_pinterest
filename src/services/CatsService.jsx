ba67548ced689efd39f1175fadebcaf00669c8cd
Efim-Kapliy <efim0424@mail.ru>
2024-02-01 18:13:35 +0300

    fix: deploy

--- src/services/CatsService.jsx
+++ src/services/CatsService.jsx
@@ -4,11 +4,12 @@ const useCatsService = () => {
   const { loading, request, error, clearError } = useHttp();
 
   const _apiBase = "https://api.thecatapi.com/v1/images/";
-  const _apiKey = import.meta.env.VITE_BASE_API_KEY;
+  // const _apiKey = import.meta.env.VITE_BASE_API_KEY;
+  const _apiKey = "live_x6cc8XljbBNFTq4x2Rj7xeAVhNY32OhQrTmLfqJWXm4ghLPwOtawagUWKHczBSbx";
   const _baseOffset = 33;
 
   const getAllCats = async (offset = _baseOffset) => {
-    const res = await request(`${_apiBase}search?limit=15&order=ASC&page=${offset}&${_apiKey}`);
+    const res = await request(`${_apiBase}search?limit=15&order=ASC&page=${offset}&api_key=${_apiKey}`);
 
     return res.map(_transformCharacter);
   };
